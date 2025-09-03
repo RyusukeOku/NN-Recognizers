dd4ecead347aa529ccbe2093c21dc59f5917cf26
RyusukeOku <ube2207r2@gmail.com>
2025-09-03 11:07:44 +0900

    fix: シリアル化できない引数を削除

--- src/recognizers/neural_networks/model_interface.py
+++ src/recognizers/neural_networks/model_interface.py
@@ -221,59 +221,59 @@ class RecognitionModelInterface(ModelInterface):
 
         return kwargs
 
-    def construct_saver(self, args, vocabulary_data=None):
-        # If loading, let the saver load kwargs from the directory.
-        # Otherwise, get kwargs from the command-line arguments.
-        if getattr(args, 'load_model', None) is not None:
-            # --- Loading Path ---
-            import json
-            from pathlib import Path
+    # def construct_saver(self, args, vocabulary_data=None):
+    #     # If loading, let the saver load kwargs from the directory.
+    #     # Otherwise, get kwargs from the command-line arguments.
+    #     if getattr(args, 'load_model', None) is not None:
+    #         # --- Loading Path ---
+    #         import json
+    #         from pathlib import Path
 
-            load_path = Path(args.load_model)
-            kwargs_path = load_path / 'kwargs.json'
-            if not kwargs_path.is_file():
-                raise FileNotFoundError(f"Cannot find kwargs.json in {load_path}")
+    #         load_path = Path(args.load_model)
+    #         kwargs_path = load_path / 'kwargs.json'
+    #         if not kwargs_path.is_file():
+    #             raise FileNotFoundError(f"Cannot find kwargs.json in {load_path}")
 
-            with open(kwargs_path, 'r') as f:
-                kwargs = json.load(f)
+    #         with open(kwargs_path, 'r') as f:
+    #             kwargs = json.load(f)
 
-            # Reconstruct non-serializable objects
-            # The vocabulary is needed to correctly initialize the model size.
-            # We get the vocabulary_data path from the training_data argument.
+    #         # Reconstruct non-serializable objects
+    #         # The vocabulary is needed to correctly initialize the model size.
+    #         # We get the vocabulary_data path from the training_data argument.
 
-            # We need to re-run parts of get_kwargs to reconstruct the vocab
-            # and other non-serialized objects.
-            # This is complex. A simpler way is to pass all necessary serializable args
-            # from the loaded kwargs to get_kwargs.
+    #         # We need to re-run parts of get_kwargs to reconstruct the vocab
+    #         # and other non-serialized objects.
+    #         # This is complex. A simpler way is to pass all necessary serializable args
+    #         # from the loaded kwargs to get_kwargs.
 
-            # Let's create a temporary args namespace from the loaded kwargs
-            temp_args = argparse.Namespace(**kwargs)
+    #         # Let's create a temporary args namespace from the loaded kwargs
+    #         temp_args = argparse.Namespace(**kwargs)
 
-            # We also need some args from the current command line
-            temp_args.training_data = args.training_data
+    #         # We also need some args from the current command line
+    #         temp_args.training_data = args.training_data
 
-            # Now call get_kwargs with the reconstructed args
-            full_kwargs = self.get_kwargs(temp_args, vocabulary_data)
+    #         # Now call get_kwargs with the reconstructed args
+    #         full_kwargs = self.get_kwargs(temp_args, vocabulary_data)
 
-            saver = construct_saver(self.construct_model, args.load_model, **full_kwargs)
-            self.on_saver_constructed(args, saver)
-        else:
-            # Training path
-            kwargs = self.get_kwargs(args, vocabulary_data)
-            output = args.output
-            saver = construct_saver(self.construct_model, output, **kwargs)
+    #         saver = construct_saver(self.construct_model, args.load_model, **full_kwargs)
+    #         self.on_saver_constructed(args, saver)
+    #     else:
+    #         # Training path
+    #         kwargs = self.get_kwargs(args, vocabulary_data)
+    #         output = args.output
+    #         saver = construct_saver(self.construct_model, output, **kwargs)
 
-            self.on_saver_constructed(args, saver)
+    #         self.on_saver_constructed(args, saver)
 
-        # Remove non-serializable arguments before saving.
-        for key in ['fsa', 'fsa_container', 'fsa_alphabet', 'word_vocab']:
-            if key in saver.kwargs:
-                del saver.kwargs[key]
+    #     # Remove non-serializable arguments before saving.
+    #     for key in ['fsa', 'fsa_container', 'fsa_alphabet', 'word_vocab']:
+    #         if key in saver.kwargs:
+    #             del saver.kwargs[key]
 
-            if 'reset_symbol_ids' in saver.kwargs and saver.kwargs['reset_symbol_ids'] is not None:
-                saver.kwargs['reset_symbol_ids'] = sorted(list(saver.kwargs['reset_symbol_ids']))
+    #         if 'reset_symbol_ids' in saver.kwargs and saver.kwargs['reset_symbol_ids'] is not None:
+    #             saver.kwargs['reset_symbol_ids'] = sorted(list(saver.kwargs['reset_symbol_ids']))
 
-        return saver
+    #     return saver
 
 
     def construct_model(self, architecture=None, **kwargs):
@@ -428,6 +428,19 @@ class RecognitionModelInterface(ModelInterface):
         smart_init(model, generator, fallback=uniform_fallback(args.init_scale))
 
     def on_saver_constructed(self, args, saver):
+        # First, let the base class do its setup
+        super().on_saver_constructed(args, saver)
+
+        # Now, remove non-serializable arguments from the kwargs that will be saved.
+        # This prevents the JSON serialization error during training,
+        # while ensuring the objects were available during model construction.
+        for key in ['fsa', 'fsa_container', 'fsa_alphabet', 'word_vocab']:
+            if key in saver.kwargs:
+                del saver.kwargs[key]
+
+        if 'reset_symbol_ids' in saver.kwargs and saver.kwargs['reset_symbol_ids'] is not None:
+            saver.kwargs['reset_symbol_ids'] = sorted(list(saver.kwargs['reset_symbol_ids']))
+
         self.bos_index = saver.kwargs['bos_index']
         self.uses_bos = self.bos_index is not None
         self.eos_index = saver.kwargs['eos_index']
